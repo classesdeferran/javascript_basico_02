e2a032f27fbdfc796e86131accf87e662e491764
Ferran <ferran.docencia@gmail.com>
2023-11-06 13:22:16 +0100

    JS varibles numéricas

---
+++ js_01.js
@@ -0,0 +1,70 @@
+// VARIABLES
+
+let numero1 = 1023 // variable de tipo entero
+console.log(typeof numero1)
+
+let numero2 = 102.3 // variable de tipo decimal
+console.log(typeof numero2)
+
+let texto1 = "Esto es un texto"
+console.log(typeof texto1)
+
+let verdadero = true
+console.log(typeof verdadero)
+
+// document.getElementById("js1").innerText = numero1
+console.log(numero1)
+
+// Variables numéricas 
+let num1 = 1
+let num2 = 2
+let num3 = 3
+let num4 = 4
+
+num1 = 300
+let suma = num1 + num2
+console.log(suma);
+let resta = num1 - num2
+console.log(resta);
+let multiplicar = num1 * num2
+console.log(multiplicar);
+let division = num1 / num2
+console.log(division);
+
+console.log(1000/0);
+
+// exponente
+console.log( 27 ** (1/3))
+
+// módulo = resto de la división 
+console.log(20%3);
+
+
+console.log("La suma es", 2344455+21351456416);
+
+let total = 1 + 2 - (3 * 4) / 5
+
+
+// Strings
+texto1 = "Buenos"
+let texto2 = "días"
+
+texto_total = texto1 + " " + texto2
+console.log(texto_total);
+
+console.log("el texto tiene", texto_total.length);
+console.log(texto_total.toUpperCase());
+console.log(texto_total.toLowerCase());
+
+let nombre = "vIcTOr peREZ".toLocaleLowerCase()
+console.log(nombre);
+
+let saludo = "Buenos días"
+// saludo = saludo - "días"
+console.log(saludo)
+
+let num5 = 5
+num5 = num5 + 4
+num5 += 4
+
+console.log("num5 vale", num5);
